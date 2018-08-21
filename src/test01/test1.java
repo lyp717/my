fed50115628ddbc925de3d1c5958ca66644a670f
li <297489846@qq.com>
2018-08-21 21:20:17 +0800

    test1添加类

---
+++ src/test01/test1.java
@@ -0,0 +1,11 @@
+package test01;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("****");
+            System.out.println("!!!!!");
+        }
+    }
+}
